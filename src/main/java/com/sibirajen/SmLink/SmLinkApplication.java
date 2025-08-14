b7a5cd0f9f3783a21e0cfcfe141125d7ffaf8450
Sibirajen <Sibirajen16@gmail.com>
2025-08-14 11:46:03 +0530

    Initial commit

---
+++ src/main/java/com/sibirajen/SmLink/SmLinkApplication.java
@@ -0,0 +1,13 @@
+package com.sibirajen.SmLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sibirajen/SmLink/SmLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sibirajen.SmLink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
