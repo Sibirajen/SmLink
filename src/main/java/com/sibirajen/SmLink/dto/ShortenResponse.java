fd65b0ea200e4770856c6d34a25df035b5e83781
Sibirajen <Sibirajen16@gmail.com>
2025-08-17 10:44:10 +0530

    DTO ShortenResponse

---
+++ src/main/java/com/sibirajen/SmLink/dto/ShortenResponse.java
@@ -0,0 +1,9 @@
+package com.sibirajen.SmLink.dto;
+
+public class ShortenResponse {
+    private Long id;
+    private String url;
+    private String shortCode;
+    private String createdAt;
+    private String updatedAt;
+}
