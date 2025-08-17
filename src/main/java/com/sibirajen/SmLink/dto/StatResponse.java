2be2dc1ba5feb238a2ec4543f6c22b07d95013cc
Sibirajen <Sibirajen16@gmail.com>
2025-08-17 10:44:17 +0530

    DTO StatResponse

---
+++ src/main/java/com/sibirajen/SmLink/dto/StatResponse.java
@@ -0,0 +1,10 @@
+package com.sibirajen.SmLink.dto;
+
+public class StatResponse {
+    private Long id;
+    private String url;
+    private String shortCode;
+    private String createdAt;
+    private String updatedAt;
+    private int accessCount;
+}
