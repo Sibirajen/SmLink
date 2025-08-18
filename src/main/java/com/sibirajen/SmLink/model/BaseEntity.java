1ec04f7111168a132a81a227d2d8d89ec4ee0c4c
Sibirajen <Sibirajen16@gmail.com>
2025-08-18 13:19:13 +0530

    Added base entity with id

---
+++ src/main/java/com/sibirajen/SmLink/model/BaseEntity.java
@@ -0,0 +1,20 @@
+package com.sibirajen.SmLink.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
+@Access(AccessType.FIELD)
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, updatable = false, nullable = false)
+    private Long id;
+}
