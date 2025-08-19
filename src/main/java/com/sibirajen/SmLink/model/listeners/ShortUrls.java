40712284c8d2019d1490f3db7a378d85952fb907
Sibirajen <Sibirajen16@gmail.com>
2025-08-19 11:13:47 +0530

    Added ShortUrls

---
+++ src/main/java/com/sibirajen/SmLink/model/listeners/ShortUrls.java
@@ -0,0 +1,25 @@
+package com.sibirajen.SmLink.model.listeners;
+
+import com.sibirajen.SmLink.model.DateEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@ToString(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "short_urls")
+@Access(AccessType.FIELD)
+public class ShortUrls extends DateEntity {
+    @Column(name = "short_code", nullable = false, unique = true, length = 20)
+    private String shortCode;
+
+    @Column(name = "original_url", nullable = false, columnDefinition = "TEXT")
+    private String originalUrl;
+
+    @Column(name = "access_count", nullable = false)
+    private int accessCount = 0;
+}
