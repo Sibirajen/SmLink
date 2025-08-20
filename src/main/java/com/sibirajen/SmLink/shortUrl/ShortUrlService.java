2652d6cc26f9a96a56f7f3575477bef090225b80
Sibirajen <Sibirajen16@gmail.com>
2025-08-20 22:13:21 +0530

    updated getShortUrl service and added updateShortUrl service

--- src/main/java/com/sibirajen/SmLink/shortUrl/ShortUrlService.java
+++ src/main/java/com/sibirajen/SmLink/shortUrl/ShortUrlService.java
@@ -1,8 +1,12 @@
 package com.sibirajen.SmLink.shortUrl;
 
+import com.sibirajen.SmLink.shortUrl.dto.Request;
 import com.sibirajen.SmLink.shortUrl.dto.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class ShortUrlService {
@@ -13,7 +17,31 @@ public class ShortUrlService {
         this.shortUrlRepo = shortUrlRepo;
     }
 
-    public Response getShortUrl(String shortCode) {
-        return null;
+    public Optional<Response> getShortUrl(String shortCode) {
+        return shortUrlRepo.findByShortCode(shortCode)
+                .map(shortUrl -> Response.builder()
+                        .id(shortUrl.getId())
+                        .url(shortUrl.getOriginalUrl())
+                        .shortCode(shortUrl.getShortCode())
+                        .createdAt(shortUrl.getCreatedAt())
+                        .updatedAt(shortUrl.getUpdatedAt())
+                        .build()
+                );
+    }
+
+    @Transactional
+    public Optional<Response> updateShortUrl(String shortCode, Request request) {
+        return shortUrlRepo.findByShortCode(shortCode)
+                .map(shortUrl -> {
+                    shortUrl.setOriginalUrl(request.getUrl());
+                    shortUrl = shortUrlRepo.save(shortUrl);
+                    return Response.builder()
+                            .id(shortUrl.getId())
+                            .url(shortUrl.getOriginalUrl())
+                            .shortCode(shortUrl.getShortCode())
+                            .createdAt(shortUrl.getCreatedAt())
+                            .updatedAt(shortUrl.getUpdatedAt())
+                            .build();
+                });
     }
 }
