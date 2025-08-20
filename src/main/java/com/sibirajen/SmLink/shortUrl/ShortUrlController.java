0abd43ad9dbfa7b88d3847edce45546014167ae7
Sibirajen <Sibirajen16@gmail.com>
2025-08-20 08:26:43 +0530

    added GET /shorten/abc123

--- src/main/java/com/sibirajen/SmLink/shortUrl/ShortUrlController.java
+++ src/main/java/com/sibirajen/SmLink/shortUrl/ShortUrlController.java
@@ -1,4 +1,26 @@
 package com.sibirajen.SmLink.shortUrl;
 
+import com.sibirajen.SmLink.shortUrl.dto.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/shorten")
 public class ShortUrlController {
+    private final ShortUrlService shortUrlService;
+
+    @Autowired
+    public ShortUrlController(ShortUrlService shortUrlService) {
+        this.shortUrlService = shortUrlService;
+    }
+
+    @GetMapping("/{shortCode}")
+    public ResponseEntity<Response> getShortUrl(@PathVariable String shortCode){
+        Response response = shortUrlService.getShortUrl(shortCode);
+        return ResponseEntity.ok(response);
+    }
 }
