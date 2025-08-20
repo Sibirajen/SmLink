0626a3457e82a295e86a015919b8a8f6dfefb27f
Sibirajen <Sibirajen16@gmail.com>
2025-08-20 08:25:52 +0530

    added findByShortCode method

--- src/main/java/com/sibirajen/SmLink/shortUrl/ShortUrlRepo.java
+++ src/main/java/com/sibirajen/SmLink/shortUrl/ShortUrlRepo.java
@@ -3,6 +3,9 @@ package com.sibirajen.SmLink.shortUrl;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ShortUrlRepo extends JpaRepository<ShortUrl, Long> {
+    Optional<ShortUrl> findByShortCode(String shortCode);
 }
