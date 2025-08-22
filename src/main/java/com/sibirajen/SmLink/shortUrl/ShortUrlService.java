515eb2cc29fdf836d9d17bd38e59794b53a403c1
Sibirajen <Sibirajen16@gmail.com>
2025-08-22 12:16:29 +0530

    Added mapToResponse and createShortUrl service method

--- src/main/java/com/sibirajen/SmLink/shortUrl/ShortUrlService.java
+++ src/main/java/com/sibirajen/SmLink/shortUrl/ShortUrlService.java
@@ -1,5 +1,6 @@
 package com.sibirajen.SmLink.shortUrl;
 
+import com.sibirajen.SmLink.common.utility.Base62Utility;
 import com.sibirajen.SmLink.shortUrl.dto.Request;
 import com.sibirajen.SmLink.shortUrl.dto.Response;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,22 +12,17 @@ import java.util.Optional;
 @Service
 public class ShortUrlService {
     private final ShortUrlRepo shortUrlRepo;
+    private final Base62Utility base62Utility;
 
     @Autowired
-    public ShortUrlService(ShortUrlRepo shortUrlRepo) {
+    public ShortUrlService(ShortUrlRepo shortUrlRepo, Base62Utility base62Utility) {
         this.shortUrlRepo = shortUrlRepo;
+        this.base62Utility = base62Utility;
     }
 
     public Optional<Response> getShortUrl(String shortCode) {
         return shortUrlRepo.findByShortCode(shortCode)
-                .map(shortUrl -> Response.builder()
-                        .id(shortUrl.getId())
-                        .url(shortUrl.getOriginalUrl())
-                        .shortCode(shortUrl.getShortCode())
-                        .createdAt(shortUrl.getCreatedAt())
-                        .updatedAt(shortUrl.getUpdatedAt())
-                        .build()
-                );
+                .map(this::mapToResponse);
     }
 
     @Transactional
@@ -34,14 +30,32 @@ public class ShortUrlService {
         return shortUrlRepo.findByShortCode(shortCode)
                 .map(shortUrl -> {
                     shortUrl.setOriginalUrl(request.getUrl());
-                    shortUrl = shortUrlRepo.save(shortUrl);
-                    return Response.builder()
-                            .id(shortUrl.getId())
-                            .url(shortUrl.getOriginalUrl())
-                            .shortCode(shortUrl.getShortCode())
-                            .createdAt(shortUrl.getCreatedAt())
-                            .updatedAt(shortUrl.getUpdatedAt())
-                            .build();
+                    return mapToResponse(shortUrl);
                 });
+    }
+
+    @Transactional
+    public Optional<Response> createShortUrl(Request request) {
+        final String originalUrl = request.getUrl();
+
+        return shortUrlRepo.findByOriginalUrl(originalUrl)
+                .map(this::mapToResponse)
+                .or(() -> {
+                   ShortUrl shortUrl = ShortUrl.builder()
+                           .originalUrl(originalUrl).build();
+                   shortUrl = shortUrlRepo.save(shortUrl);
+                   shortUrl.setShortCode(base62Utility.encode(shortUrl.getId()));
+                   return Optional.of(mapToResponse(shortUrl));
+                });
+    }
+
+    private Response mapToResponse(ShortUrl shortUrl) {
+        return Response.builder()
+                .id(shortUrl.getId())
+                .url(shortUrl.getOriginalUrl())
+                .shortCode(shortUrl.getShortCode())
+                .createdAt(shortUrl.getCreatedAt())
+                .updatedAt(shortUrl.getUpdatedAt())
+                .build();
     }
 }
