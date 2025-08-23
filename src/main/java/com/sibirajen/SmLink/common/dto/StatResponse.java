280d67af4077167bcb6312296ea7463db22ab40d
Sibirajen <Sibirajen16@gmail.com>
2025-08-23 12:46:56 +0530

    Added lombok annotations

--- src/main/java/com/sibirajen/SmLink/common/dto/StatResponse.java
+++ src/main/java/com/sibirajen/SmLink/common/dto/StatResponse.java
@@ -1,10 +1,21 @@
-package com.sibirajen.SmLink.dto;
+package com.sibirajen.SmLink.common.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class StatResponse {
     private Long id;
     private String url;
     private String shortCode;
-    private String createdAt;
-    private String updatedAt;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
     private int accessCount;
 }
