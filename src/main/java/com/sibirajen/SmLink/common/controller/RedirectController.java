dba2949a7b25b0bebc76b073bcfd1a266b486d6b
Sibirajen <Sibirajen16@gmail.com>
2025-08-26 11:36:04 +0530

    Added redirect controller

--- src/main/java/com/sibirajen/SmLink/common/controller/RedirectController.java
+++ src/main/java/com/sibirajen/SmLink/common/controller/RedirectController.java
@@ -2,6 +2,8 @@ package com.sibirajen.SmLink.common.controller;
 
 import com.sibirajen.SmLink.shortUrl.ShortUrlService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,26 +22,13 @@ public class RedirectController {
 
     @GetMapping("/{shortCode}")
     public ResponseEntity<Void> reDirect(@PathVariable String shortCode){
-        return ResponseEntity.ok().build();
+        Optional<String> optionalOriginalUrl = shortUrlService.getLongUrl(shortCode);
+        return optionalOriginalUrl.map(url -> ResponseEntity
+                        .status(HttpStatus.FOUND)
+                        .header(HttpHeaders.LOCATION, url)
+                        .<Void>build())
+                .orElseGet(() -> ResponseEntity
+                        .status(HttpStatus.NOT_FOUND)
+                        .build());
     }
 }
-
-//        if (optionalOriginalUrl.isEmpty()){
-//            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-//        }
-//
-//        return ResponseEntity
-//                .status(HttpStatus.PERMANENT_REDIRECT)
-//                .header(HttpHeaders.LOCATION, optionalOriginalUrl.get())
-//                .build();
-
-
-//Optional<String> optionalOriginalUrl = shortUrlService.getLongUrl(shortCode);
-//
-//        return optionalOriginalUrl.map(url -> ResponseEntity
-//        .status(HttpStatus.FOUND)
-//                    .header(HttpHeaders.LOCATION, url)
-//                    .<Void>build())
-//        .orElseGet(() -> ResponseEntity
-//        .status(HttpStatus.NOT_FOUND)
-//                        .build());
